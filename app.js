09adaf51da9fd6c1f727fb675a67518ee56a8ed3
Mahmud Toshtemirov <mahmuddev1908@gmail.com>
2024-01-24 13:31:06 +0500

    responsive

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerButton = document.getElementById("burgerButton");
+  const navigation = document.querySelector(".navigation");
+  const navItems = document.querySelectorAll(".navigation li");
+
+  // Toggle navigation menu on burger button click
+  burgerButton.addEventListener("click", function () {
+    navigation.classList.toggle("show-nav");
+  });
+
+  // Close navigation menu when a list item is clicked
+  navItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      navigation.classList.remove("show-nav");
+    });
+  });
+});
